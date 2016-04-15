efbc5d04408208a97ffcc746eec392a8ae7e7397
lennartVH01 <lennart.vanhirtum@gmail.com>
2016-04-15 08:18:15 +0200

    Added getItemStacks to ItemUtils

--- src/me/lennartVH01/ItemUtils.java
+++ src/me/lennartVH01/ItemUtils.java
@@ -1,6 +1,10 @@
 package me.lennartVH01;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.bukkit.inventory.ItemStack;
 
 
 public class ItemUtils {
@@ -16,5 +20,24 @@ public class ItemUtils {
 			}
 		}
 		return true;
+	}
+	public static ItemStack[] getItemStacks(ItemStack item){
+		return getItemStacks(item, item.getAmount());
+	}
+	public static ItemStack[] getItemStacks(ItemStack item, int maxTakeOff){
+		ItemStack[] stacks = new ItemStack[(maxTakeOff+63) / 64]; //take ceil of itemStack size/64
+		
+		item.setAmount(item.getAmount() - maxTakeOff);
+		for(int i = 0; i < maxTakeOff / 64; i++){
+			stacks[i] = item.clone();
+			stacks[i].setAmount(64);
+		}
+		int remainder = maxTakeOff % 64;
+		if(remainder != 0){
+			stacks[stacks.length] = item.clone();
+			stacks[stacks.length].setAmount(remainder);
+			
+		}
+		return stacks;
 	}
 }
